24f26dc09cd7fa1a36a24f2a7ec41a6e2ad0f38f
Simon Haisz <simonhaisz@gmail.com>
2019-02-09 11:08:03 -0500

    Updates

---
+++ precache-manifest.fa0d877bd66d2a004ba1bee6ab5e3a47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c867013e9ab4efa6db5c",
+    "url": "/map-labeller/static/css/main.4e02741e.chunk.css"
+  },
+  {
+    "revision": "c867013e9ab4efa6db5c",
+    "url": "/map-labeller/static/js/main.c867013e.chunk.js"
+  },
+  {
+    "revision": "2ff7d33ae469ca6408be",
+    "url": "/map-labeller/static/js/1.2ff7d33a.chunk.js"
+  },
+  {
+    "revision": "23bd06555fa98d331bef",
+    "url": "/map-labeller/static/js/runtime~main.23bd0655.js"
+  },
+  {
+    "revision": "7a103981a2248bf21aa7604943247e56",
+    "url": "/map-labeller/index.html"
+  }
+];
