da0b6e9f0ef800b0176f497da7a0b37ec592d094
Simon Haisz <simonhaisz@gmail.com>
2019-02-09 11:24:18 -0500

    Updates

---
+++ precache-manifest.de39ae87180b02bd07722b1b2047ce55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a513e6fc77d5c0226f90",
+    "url": "/middle-earth/static/css/main.4e02741e.chunk.css"
+  },
+  {
+    "revision": "a513e6fc77d5c0226f90",
+    "url": "/middle-earth/static/js/main.a513e6fc.chunk.js"
+  },
+  {
+    "revision": "2ff7d33ae469ca6408be",
+    "url": "/middle-earth/static/js/1.2ff7d33a.chunk.js"
+  },
+  {
+    "revision": "2b06e1c677160ed09415",
+    "url": "/middle-earth/static/js/runtime~main.2b06e1c6.js"
+  },
+  {
+    "revision": "8a490c3ba272c4ad44d8f5af7a7375bc",
+    "url": "/middle-earth/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/map-labeller/precache-manifest.4717700e59d2f994f6021618d1d46c1d.js"
+  "/middle-earth/precache-manifest.de39ae87180b02bd07722b1b2047ce55.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/map-labeller/index.html", {
+workbox.routing.registerNavigationRoute("/middle-earth/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
